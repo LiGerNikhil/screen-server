f01bbd8f6074470b596432f19194e8974ce6ade6
Nikhil kumar <112514956+LiGerNikhil@users.noreply.github.com>
2025-07-15 04:14:14 +0530

    Fix Render production config

--- server.py
+++ server.py
@@ -82,5 +82,5 @@ def process_frame(b64_img):
 
 # === Run Server ===
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 10000))
     socketio.run(app, host="0.0.0.0", port=port)
